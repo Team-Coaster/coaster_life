7b0a20116ae80662e7505849f829d9cf3b9f4cf3
devfaraday <farayibitobi@gmail.com>
2021-11-30 22:53:55 -0600

    User Location Display

--- app/src/main/java/com/teamcoaster/coasterlife/Fragments/PeopleFragment.java
+++ app/src/main/java/com/teamcoaster/coasterlife/Fragments/PeopleFragment.java
@@ -2,17 +2,27 @@ package com.teamcoaster.coasterlife.Fragments;
 
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -23,6 +33,8 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.teamcoaster.coasterlife.R;
 
 import java.util.Map;
@@ -31,6 +43,10 @@ public class PeopleFragment extends Fragment {
 
     MapView mMapView;
     private GoogleMap googleMap;
+    private Geocoder geocoder;
+    private int ACCESS_LOCATION_REQUEST_CODE = 1001;
+    public static final String TAG = "PeopleFragment";
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     public PeopleFragment() {
         // Required empty public constructor
@@ -43,8 +59,8 @@ public class PeopleFragment extends Fragment {
 
         mMapView = (MapView) rootView.findViewById(R.id.mapView);
         mMapView.onCreate(savedInstanceState);
-
-        mMapView.onResume(); // needed to get the map to display immediately
+        // Gets the map to display immediately
+        mMapView.onResume();
 
         try {
             MapsInitializer.initialize(getActivity().getApplicationContext());
@@ -61,18 +77,53 @@ public class PeopleFragment extends Fragment {
                 //googleMap.setMyLocationEnabled(true);
 
                 // For dropping a marker at a point on the Map
-                LatLng sydney = new LatLng(-34, 151);
-                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+                LatLng dallas = new LatLng(32.776, -96.796);
+                googleMap.addMarker(new MarkerOptions().position(dallas).title("Marker Title").snippet("Marker Description"));
+
+                geocoder = new Geocoder(getActivity());
+                fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+
+                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                    enableUserLocation();
+//            googleMap.setMyLocationEnabled(true);
+                    zoomToUserLocation();
+
+                } else {
+                    if(ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)){
+                        //Show User dialog why this permission is necessary
+                        ActivityCompat.requestPermissions(getActivity(), new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, ACCESS_LOCATION_REQUEST_CODE);
+                    }else{
+                        ActivityCompat.requestPermissions(getActivity(), new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, ACCESS_LOCATION_REQUEST_CODE);
+                    }
+                }
 
                 // For zooming automatically to the location of the marker
-                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
-                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+//                CameraPosition cameraPosition = new CameraPosition.Builder().target(dallas).zoom(12).build();
+//                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
             }
         });
+
+
 
         return rootView;
     }
 
+    @SuppressLint("MissingPermission")
+    private void enableUserLocation(){
+        googleMap.setMyLocationEnabled(true);
+    }
+
+    private void zoomToUserLocation(){
+        @SuppressLint("MissingPermission")
+        Task<Location> locationTask = fusedLocationProviderClient.getLastLocation();
+        locationTask.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 20));
+            }
+        });
+    }
     @Override
     public void onResume() {
         super.onResume();
@@ -96,4 +147,30 @@ public class PeopleFragment extends Fragment {
         super.onLowMemory();
         mMapView.onLowMemory();
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            enableUserLocation();
+            zoomToUserLocation();
+        } else {
+            // We can show a dialog that permission is not grated
+        }
+    }
+//    ALTERNATIVE TO DEPRECATED onRequestPermissionResult
+//    private ActivityResultLauncher<String> mPermissionResult = registerForActivityResult(
+//            new ActivityResultContracts.RequestPermission(),
+//            new ActivityResultCallback<Boolean>() {
+//                @Override
+//                public void onActivityResult(Boolean result) {
+//                    if(result) {
+//                        Log.e(TAG, "onActivityResult: PERMISSION GRANTED");
+//                        enableUserLocation();
+//                        zoomToUserLocation();
+//                    } else {
+//                        Log.e(TAG, "onActivityResult: PERMISSION DENIED");
+//                    }
+//                }
+//            });
 }
